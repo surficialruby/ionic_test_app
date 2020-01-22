8458ce70105f35c6160106585f5c25fab3a21378
atte kangasvieri <atte.kangasvieri@gmail.com>
2020-01-22 15:10:13 +0200

    Sidemenu commenting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,9 @@ export class AppComponent {
     });
   }
 
+  /*
+  * Create Navigation array
+  */
   sideMenu()
   {
     this.navigate =
@@ -46,4 +49,5 @@ export class AppComponent {
       },
     ]
   }
+
 }
