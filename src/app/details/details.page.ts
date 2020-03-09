b48c221b1c45e5118db7244fdd6dea21d36dcf8b
atte kangasvieri <atte.kangasvieri@gmail.com>
2020-03-09 16:10:26 +0200

    task details

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
+  title: string
+  description: string
 
-  constructor() { }
+  constructor(
+    public activatedRoute: ActivatedRoute,
+    public taskservice: TaskService
+  ) { }
 
   ngOnInit() {
+    const id: number = parseInt(this.activatedRoute.snapshot.paramMap.get('id'))
+    let display: any = this.taskservice.get_element(id)
+
+    this.title = display.title
+    this.description = display.description
   }
 
 }
